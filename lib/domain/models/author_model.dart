1445cd9f676579572ea8b6e537e93c0080165bf4
Karolina Eberst <karolinaeberst@gmail.com>
2023-01-17 20:58:46 +0100

    Add retrofit to author i article data source

--- lib/domain/models/author_model.dart
+++ lib/domain/models/author_model.dart
@@ -1,25 +1,16 @@
-import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'author_model.freezed.dart';
 part 'author_model.g.dart';
 
-@JsonSerializable()
-
-
-class AuthorModel {
-  const AuthorModel({
-    required this.id,
-    required this.picture,
-    required this.firstName,
-    required this.lastName,
-  });
-
-  final int id;
-  final String picture;
-
-  @JsonKey(name: 'first_name') //jsonkey dodajemy tylko dla zmiennych, które mają inną nazwę zmiennej niż pole w api
-  final String firstName;
-
-  @JsonKey(name: 'last_name')
-  final String lastName;
+@freezed
+class AuthorModel with _$AuthorModel {
+  const AuthorModel._();
+  factory AuthorModel(
+    int id,
+    String picture,
+    @JsonKey(name: 'first_name') String firstName,
+    @JsonKey(name: 'last_name') String lastName,
+  ) = _AuthorModel;
 
   String get name {
     return '$firstName $lastName';
@@ -27,6 +18,6 @@ class AuthorModel {
 
   factory AuthorModel.fromJson(Map<String, dynamic> json) =>
       _$AuthorModelFromJson(json);
-      
-  Map<String, dynamic> toJson() => _$AuthorModelToJson(this);
 }
+
+//   @JsonKey(name: 'first_name') //jsonkey dodajemy tylko dla zmiennych, które mają inną nazwę zmiennej niż pole w api
